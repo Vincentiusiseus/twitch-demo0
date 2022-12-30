e1f689a0537ac31e1c3a725b0a7ff281ca8111ee
Vincentius P41siseus <vincentiusiseus@gmail.com>
2022-12-30 13:22:04 +0900

    Fix typo & close connection

--- features/get_all_streams.ts
+++ features/get_all_streams.ts
@@ -4,16 +4,17 @@ import { getAllStreams } from "~/src/twitch-api"
 import { getClientId, getOAuthToken } from "~/src/util"
 
 async function main() {
-    const key = getClientId()
-    const secret = getOAuthToken()
+    const client_id = getClientId()
+    const access_token = getOAuthToken()
 
-    const all_streams = await getAllStreams(key, secret)
+    const all_streams = await getAllStreams(client_id, access_token)
 
     const inst = new MainDB()
     await inst.init()
     await inst.storeAllStreams(all_streams)
 
     console.log("get_all_streams: Done")
+    await inst.client.close()
 }
 
 main()
